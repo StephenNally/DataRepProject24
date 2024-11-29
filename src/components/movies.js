541c2731c0503ec48aaa5127cddbcf1f43fdd63e
Stephen Nally <g00418614@atu.ie>
2024-11-29 12:04:29 +0000

    refresh list after deletion

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,11 +1,18 @@
 import MovieItem from "./movieitem";
 
-const Movies = (props)=>{
-    return props.myMovies.map(
-        (movie)=>{
-            return <MovieItem mymovie={movie} key={movie._id} />
-        }
+function Movies(props) {
+    return (
+        <>
+            {props.myMovies.map((movie) => (
+                <MovieItem
+                    mymovie={movie}
+                    key={movie._id}
+                    Reload={props.ReloadData}
+                />
+            ))}
+        </>
     );
 }
 
 export default Movies;
+
